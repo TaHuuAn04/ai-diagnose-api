663f39606ae3e3f35570498bd370471968a080c8
TuanJerry <nguyencaotuan2004@gmail.com>
2026-05-22 20:09:23 +0700

    fix: disable throttle in Login

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -81,7 +81,7 @@ export class AuthController {
     return await this.commandBus.execute(command);
   }
 
-  @Throttle({ default: { limit: 10, ttl: 60000 } })
+  // @Throttle({ default: { limit: 10, ttl: 60000 } })
   @Post('login')
   @IsPublic()
   async login(@Body() dto: LoginBodyDto): Promise<LoginResponseDto> {
